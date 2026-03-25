6eec17a36885391395d01c35a12294fab9698834
pippahtlin <pippalinht@gmail.com>
2026-03-25 14:39:51 -0500

    test

--- mistral_test.py
+++ mistral_test.py
@@ -1,13 +1,14 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
-
-model_id = "mistralai/Mistral-7B-Instruct-v0.1"
+torch.cuda.empty_cache()
+model_id = "mistralai/Mistral-7B-Instruct-v0.3"
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(
     model_id,
     torch_dtype=torch.float16,
     device_map="auto",
+    load_in_4bit=True
 )
 
 print("loaded")
